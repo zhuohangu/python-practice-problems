1d547a5cfcd9ba470146d7ea2af87c075362e012
Zhuohan Gu <zhuohan@linux7.cs.uchicago.edu>
2023-02-06 22:37:01 -0600

    negate_list

--- problems/lists/negate_list.py
+++ problems/lists/negate_list.py
@@ -3,11 +3,9 @@ def negate_list(lst):
     Produce a *new* list with its values negated
     """
 
-    ### EXERCISE 6 -- YOUR CODE GOES HERE
-    # Replace the following line with your code.
-    # After running your code, variable n should contain the value
-    # we ask you to compute in this exercise
-    new_lst = None
+    new_lst = []
+    for val in lst:
+        new_lst.append(-val)
 
     ### DO NOT MODIFY THE FOLLOWING LINE!
     return new_lst
